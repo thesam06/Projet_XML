6142b1b871d9a7698430c423516021da986b6e21
ablohadeola <ablohadeola@gmail.com>
2017-12-22 00:57:22 +0100

    Le server fonctionne

--- src/main/java/bastriCris/org/inria/fr/ns/cr/ObjectFactory.java
+++ src/main/java/bastriCris/org/inria/fr/ns/cr/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.11 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2017.12.19 à 10:33:11 PM CET 
+// Généré le : 2017.12.22 à 12:48:39 AM CET 
 //
 
 
---
+++ src/main/java/fr/unice/miage/m1/alichamlausam/Raisin.java
@@ -0,0 +1,19 @@
+package fr.unice.miage.m1.alichamlausam;
+
+import javax.ws.rs.*;
+
+// @Path("/raisins")
+public class Raisin {
+
+	/*@GET
+	@Produces("text/plain")
+	public String delicieux() {
+		return "Je mange des raisins";
+	}*/
+
+
+
+
+}
+
+
