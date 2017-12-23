1a4f3a79c1f41ee68e70ff98391ccf4b90630fed
ablohadeola <ablohadeola@gmail.com>
2017-12-23 04:52:51 +0100

    La connexion à la base de données fonctionne

--- src/main/java/bastri/org/inria/fr/ns/sr/package-info.java
+++ src/main/java/bastri/org/inria/fr/ns/sr/package-info.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.11 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2017.12.22 à 12:48:39 AM CET 
+// Généré le : 2017.12.22 à 11:33:54 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://ns.inria.fr/dsi/si/app/bastri", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
