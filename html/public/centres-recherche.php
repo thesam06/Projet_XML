b3a9c0c966ba6872910b23cf70b6e0aa3a6b1648
Laure Vincent-Cuaz <laure.vincentcuaz@gmail.com>
2018-01-02 12:55:50 +0100

    Ajout de la partie client web

---
+++ html/public/centres-recherche.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <meta name="description" content="XML project on Raweb Inria">
+  <meta name="author" content="Laure Vincent-Cuaz">
+
+  <title>Raweb Inria - Centres</title>
+
+  <!-- CSS files -->
+  <link rel="stylesheet" href="../library/bootstrap-4.0.0/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
+  <link rel="stylesheet" href="./css/style.css" rel="stylesheet">
+
+</head>
+<body>
+  <nav class="navbar navbar-dark bg-primary navbar-toggleable-md navbar-expand-md navbar-inverse fixed-top">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Raweb Inria</a>
+
+    <div class="collapse navbar-collapse" id="mainNavbar">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">Accueil</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="centres-recherche.html">Centres de recherche</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="equipes.html">Équipes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="personnels.html">Personnels d'équipes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="statistiques.html">Données statistiques</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6">
+        <table class="table table-responsive table-bordered table-hover table-sm">
+          <thead>
+            <tr>
+              <th class="align-middle" scope="col">#</th>
+              <th class="text-center align-middle" scope="col">Nom</th>
+              <th class="text-center align-middle" scope="col">Ville</th>
+              <th class="text-center" scope="col">Date d'ouverture</th>
+              <th class="text-center" scope="col">Date de fermeture</th>
+              <th class="text-center" scope="col">Nombre d'équipes</th>
+              <th class="text-center" scope="col">Nombre de personnes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-lg-6">
+        <div id="researchCentersMap" style="width:auto;min-height:560px;"></div>
+      </div>
+    </div>
+  </div>
+
+  <!-- JS files -->
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="../library/jQuery-1.12.3/jquery-1.12.3.min.js"></script>
+  <script type="text/javascript" src="../library/bootstrap-4.0.0/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="DataTables/datatables.min.js"></script>
+
+  <script>
+  function initMapCenters() {
+    var centreLat = 46.5;
+    var centreLng = 5;
+    var latlng = new google.maps.LatLng(centreLat, centreLng);
+    var zoomMap = 5;
+    var options = {
+      zoom: zoomMap,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var researchCentersMap = new google.maps.Map(document.getElementById("researchCentersMap"), options);
+
+    var marqueur = new google.maps.Marker({
+      position: new google.maps.LatLng(43.696, 7.289429),
+      map: researchCentersMap,
+      title: "Nice"
+    });
+    var marqueur1 = new google.maps.Marker({
+      position: new google.maps.LatLng(48.854733, 2.350879),
+      map: researchCentersMap,
+      title: "Paris"
+    });
+  }
+</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWVVmEYiKBHdXFOYh_6iDMWoHQ02lHGpw&callback=initMapCenters"></script>
+</body>
+</html>
